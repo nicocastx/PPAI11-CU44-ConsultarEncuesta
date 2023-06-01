35365cc0c6fddb9c39bd6eb2726159ea10d0bc80
Martin Pignata <83866@sistemas.frc.utn.edu.ar>
2023-06-01 20:02:45 -0300

    avance en cliente y cambioEstado

--- PPAI11-CU44-ConsultarEncuesta/Entidades/CambioEstado.cs
+++ PPAI11-CU44-ConsultarEncuesta/Entidades/CambioEstado.cs
@@ -8,5 +8,24 @@ namespace PPAI11_CU44_ConsultarEncuesta.Entidades
 {
     internal class CambioEstado
     {
+        public string FechaHoraInicio { get; set; }
+        public Estado Estado { get; set; }
+
+        /* GENTE ACA IMPLEMENTE LA FECHA CON EL TIPO DATETIME, NO SABIA CUAL IMPLEMENTAR
+        public DateTime fecha;
+        public CambioEstado()
+        {
+            fecha = DateTime.Now;
+        }
+
+        public DateTime fechaHoraInicio {
+            get { return fecha; }
+            set { fecha = value; }
+        }*/
+
+        public bool esEstadoInicial()
+        {
+            return Estado.esIniciada();
+        }
     }
 }
--- PPAI11-CU44-ConsultarEncuesta/Entidades/Cliente.cs
+++ PPAI11-CU44-ConsultarEncuesta/Entidades/Cliente.cs
@@ -8,5 +8,9 @@ namespace PPAI11_CU44_ConsultarEncuesta.Entidades
 {
     internal class Cliente
     {
+        public int Dni { get; set; }
+        public string NombreCompleto { get; set; }
+        public int NroCelular { get; set; }
+
     }
 }
