77674ea94d6470215c1a29cadd7f308770ae4cdb
nicocastx <nicocastx@gmail.com>
2023-06-01 19:04:05 -0300

    Definicion estructura proyecto

---
+++ PPAI11-CU44-ConsultarEncuesta/Entidades/Estado.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI11_CU44_ConsultarEncuesta.Entidades
+{
+    internal class Estado
+    {
+        public string Nombre { get; set; }
+
+        public bool esIniciada()
+        {
+            if(this.Nombre == "Iniciada")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ PPAI11-CU44-ConsultarEncuesta/Entidades/RespuestaPosible.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI11_CU44_ConsultarEncuesta.Entidades
+{
+    internal class RespuestaPosible
+    {
+    }
+}
