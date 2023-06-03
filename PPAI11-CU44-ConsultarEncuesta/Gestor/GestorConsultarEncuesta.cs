03a7e001c327759ba9ced9100c7e02da4ec16f22
Martin Pignata <83866@sistemas.frc.utn.edu.ar>
2023-06-02 21:05:34 -0300

    avance atributos del gestor

--- PPAI11-CU44-ConsultarEncuesta/Gestor/GestorConsultarEncuesta.cs
+++ PPAI11-CU44-ConsultarEncuesta/Gestor/GestorConsultarEncuesta.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PPAI11_CU44_ConsultarEncuesta.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,60 @@ namespace PPAI11_CU44_ConsultarEncuesta.Gestor
 {
     internal class GestorConsultarEncuesta
     {
+        public DateTime fechaInicio { get; set; }
+        public DateTime fechaFin { get; set; }
+        public bool esDePeriodo { get; set; }
+        public List<Llamada> llamadasDePeriodo { get; set; }
+        public string nombreCliente { get; set; }
+        public int duracion { get; set; }
+        public List<RespuestaPosible> respuestas { get; set; }
+        public List<Pregunta> preguntas { get; set; }
+
+        public GestorConsultarEncuesta(DateTime fechaInicio, DateTime fechaFin, bool esDePeriodo, List<Llamada> llamadasDePeriodo, string nombreCliente, int duracion, List<RespuestaPosible> respuestas, List<Pregunta> preguntas)
+        {
+            this.fechaInicio = fechaInicio;
+            this.fechaFin = fechaFin;
+            this.esDePeriodo = esDePeriodo;
+            this.llamadasDePeriodo = llamadasDePeriodo;
+            this.nombreCliente = nombreCliente;
+            this.duracion = duracion;
+            this.respuestas = respuestas;
+            this.preguntas = preguntas;
+        }
+
+        public void consultarEncuesta() { 
+        }
+
+        public void tomarFechaInicio()
+        {
+        }
+
+        public void tomarFechaFin()
+        {
+        }
+
+        public void filtrarPorPeriodo()
+        {
+        }
+
+        public void filtrarQueTenganEncuestas()
+        {
+        }
+
+        public void tomarOpcionLlamada()
+        {
+        }
+
+        public void mostrarDatosLlamada()
+        {
+        }
+
+        public void tomarOpcionGenerarCSV()
+        {
+        }
+
+        public void generarArchivoCSV()
+        {
+        }
     }
 }
