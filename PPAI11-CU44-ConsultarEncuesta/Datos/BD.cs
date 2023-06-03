f040eba19182e64b99722cc8138e5e3ce047760f
nicocastx <nicocastx@gmail.com>
2023-06-03 17:08:19 -0300

    asd123

--- PPAI11-CU44-ConsultarEncuesta/Datos/BD.cs
+++ PPAI11-CU44-ConsultarEncuesta/Datos/BD.cs
@@ -9,26 +9,24 @@ namespace PPAI11_CU44_ConsultarEncuesta.Datos
 {
     public class BD
     {
-        static string descripcionRP15 = "Ingrese un valor del 1 al 5 donde: 1-Completamente Insatisfecho, 2-Poco Satisfecho, 3-Satisfecho, 4-Muy Satisfecho, 5-Altamente Satisfecho.";
-        static string descripcionRP12 = "Ingrese el 1 para marcar 'Sí' o ingrese el 2 para marcar 'No'.";
-
-        /*Objetos Cliente*/
         public static List<Cliente> ListaClientes()
         {
-            List<Cliente> clientes = new List<Cliente>();
-            Cliente clienteUno = new Cliente(43881554, "Fransico Ahumada", 352659835);
-            Cliente clienteDos = new Cliente(42114678, "Ernesto Ducart", 351657866);
-            Cliente clienteTres = new Cliente(4177953, "Agustin Ortega", 351597243);
-            Cliente clienteCuatro = new Cliente(43881554, "Agostina Beletti", 351237966);
-            Cliente clienteCinco = new Cliente(43224678, "Leandro Colmenares", 351457292);
+            List<Cliente> listaClientes = new List<Cliente>();
+            Cliente clienteUno = new Cliente(Dni: 23123133,NombreCompleto: "asd", NroCelular: 231123123);
+            Cliente clienteDos = new Cliente(Dni: 23123133, NombreCompleto: "asd", NroCelular: 231123123);
+            listaClientes.Add(clienteUno);
 
-            clientes.Add(clienteUno);
-            clientes.Add(clienteDos);
-            clientes.Add(clienteTres);
-            clientes.Add(clienteCuatro);
-            clientes.Add(clienteCinco);
+            return listaClientes;
+        }
 
-            return clientes;
+        public static List<Cliente> ListaClientes()
+        {
+            List<Cliente> listaClientes = new List<Cliente>();
+            Cliente clienteUno = new Cliente(Dni: 23123133, NombreCompleto: "asd", NroCelular: 231123123);
+            Cliente clienteDos = new Cliente(Dni: 23123133, NombreCompleto: "asd", NroCelular: 231123123);
+            listaClientes.Add(clienteUno);
+
+            return listaClientes;
         }
     }
 }
