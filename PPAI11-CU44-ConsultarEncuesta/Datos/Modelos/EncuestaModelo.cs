eed130f7468be70c62df3fc150f56722098ece70
nicocastx <nicocastx@gmail.com>
2023-11-10 15:36:13 -0300

    base de datos

---
+++ PPAI11-CU44-ConsultarEncuesta/Datos/Modelos/EncuestaModelo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPAI11_CU44_ConsultarEncuesta.Datos.Modelos
+{
+    internal class EncuestaModelo
+    {
+        private RepositorioBD repo = RepositorioBD.ObtenerInstancia();
+        private String Tabla = "Encuesta";
+
+
+    }
+}
